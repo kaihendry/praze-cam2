7b69279cda8b242bc0a203d78f963bd69eb4ecbf
Kai Hendry <hendry@webconverger.com>
2019-07-29 14:36:16 +0800

    Check IP address

--- go.mod
+++ go.mod
@@ -3,13 +3,9 @@ module github.com/kaihendry/videos.natalian.org
 go 1.12
 
 require (
-	github.com/BurntSushi/toml v0.3.1
 	github.com/apex/log v1.1.1
-	github.com/aws/aws-sdk-go v1.21.0
-	github.com/aws/aws-sdk-go-v2 v0.9.0
+	github.com/aws/aws-sdk-go-v2 v0.10.0
 	github.com/gorilla/mux v1.7.3
-	github.com/gorilla/schema v1.1.0 // indirect
-	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
-	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
-	google.golang.org/api v0.7.0
+	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
+	golang.org/x/text v0.3.2 // indirect
 )
--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"html/template"
+	"net"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/apex/log"
+	"github.com/apex/log/handlers/json"
+	"github.com/apex/log/handlers/text"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -17,12 +23,18 @@ import (
 
 type YoutubeBackup struct {
 	FilePath string
-	URL      string
 }
 
 var views = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+
+	if os.Getenv("UP_STAGE") == "" {
+		log.SetHandler(text.Default)
+	} else {
+		log.SetHandler(json.Default)
+	}
+
 	addr := ":" + os.Getenv("PORT")
 	app := mux.NewRouter()
 	app.HandleFunc("/", today)
@@ -36,7 +48,36 @@ func today(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, fmt.Sprintf("/v?date=%s", time.Now().AddDate(0, 0, -1).Format("2006-01-02")), http.StatusFound)
 }
 
+func allowed(r *http.Request) error {
+	if r.Header.Get("X-Forwarded-For") == "" {
+		return errors.New("missing X-Forwarded-For")
+	}
+
+	remoteAddr := strings.Split(r.Header.Get("X-Forwarded-For"), ",")[0]
+
+	whitelist := []string{
+		"81.187.180.129/26",
+		"210.23.22.2/32",
+	}
+
+	for _, network := range whitelist {
+		_, subnet, _ := net.ParseCIDR(network)
+		ip := net.ParseIP(remoteAddr)
+		if subnet.Contains(ip) {
+			return nil
+		}
+	}
+	return fmt.Errorf("%s not in whitelist", remoteAddr)
+}
+
 func showVideos(w http.ResponseWriter, r *http.Request) {
+	if err := allowed(r); err != nil {
+		log.WithError(err).Info("not allowed")
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	log.WithField("remoteaddr", r.Header.Get("X-Forwarded-For")).Info("from")
 	date := r.FormValue("date")
 	ctx := log.WithFields(log.Fields{
 		"date": date,
@@ -65,15 +106,10 @@ func showVideos(w http.ResponseWriter, r *http.Request) {
 	for p.Next(context.TODO()) {
 		page := p.CurrentPage()
 		for _, obj := range page.Contents {
-			req := svc.GetObjectRequest(&s3.GetObjectInput{
-				Bucket: aws.String("c.prazefarm.co.uk"),
-				Key:    obj.Key,
-			})
-			urlStr, err := req.Presign(15 * time.Minute)
-			if err != nil {
-				log.WithError(err).Fatal("failed to sign url")
+			ext := filepath.Ext(*obj.Key)
+			if ext == ".mp4" {
+				listing = append(listing, YoutubeBackup{FilePath: strings.TrimSuffix(*obj.Key, ext)})
 			}
-			listing = append(listing, YoutubeBackup{FilePath: *obj.Key, URL: urlStr})
 		}
 	}
 
